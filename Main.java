d4432fbdaa9bff425fc784b4f134015e6698f15a
Noah Konstantin Bentele <noah.bentele@agh-hbg.de>
2026-06-12 14:52:10 +0200

    Feature hinzugefügt

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub!");
         System.out.println("Mein zweiter Commit");
+        System.out.println("Feature-Branch aktiv");
     }
 } 
